995c3ea050289dfac59f534901ac2457239c3131
Falasefemi2 <femifalase228@gmail.com>
2024-06-11 09:07:01 +0100

    updating the line

--- app/components/MobileNavbar.tsx
+++ app/components/MobileNavbar.tsx
@@ -30,8 +30,8 @@ const MobileNav = () => {
                     <SheetTrigger className="w-[30px] h-[30px] bg-[#5F3AFB] rounded-full flex items-center justify-center">
                         <Menu className="text-white w-[15px] h-[15px]" />
                     </SheetTrigger>
-                    <SheetContent className="w-full flex flex-col justify-between">
-                        <div className="flex flex-col items-start w-[364px] px-4 mt-10">
+                    <SheetContent className="w-full flex flex-col justify-between pb-0">
+                        <div className="flex flex-col items-start px-4 mt-10">
                             <NavigationMenu className="flex flex-col items-center space-y-10">
                                 <NavigationMenuItem>
                                     <NavigationMenuTrigger>Products</NavigationMenuTrigger>
@@ -47,7 +47,9 @@ const MobileNav = () => {
                                     </Link>
                                 </NavigationMenuItem>
                             </NavigationMenu>
-                            <Separator className="text-[#696969] w-full" />
+                        </div>
+                        <div className="">
+                            <Separator className="bg-[#696969] w-full mt-0" />
                         </div>
                         <div className="flex justify-center px-4">
                             <Button size="lg">Get Started</Button>
