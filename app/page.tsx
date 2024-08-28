ff62d52adda25e09491e010f4c7d0884131685fd
Falasefemi2 <femifalase228@gmail.com>
2024-08-28 11:58:35 +0100

    remove import

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 
-import { Component } from "./components/FadeImage";
 import HeroSection from "./components/HeroSection";
 import ImageSlider from "./components/ImageSlider";
 import RelatedSearch from "./components/RelatedSearch";
@@ -11,7 +10,6 @@ export default function Home() {
   return (
     <>
       <HeroSection />
-      {/* <Component /> */}
       <ImageSlider />
       <RelatedSearch />
       <TrendingProducts />
