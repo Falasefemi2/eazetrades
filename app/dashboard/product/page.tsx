e67c7a58956a8aabcf843c0bbc996278b90321be
Falasefemi2 <femifalase228@gmail.com>
2024-09-19 17:16:22 +0100

    svg fixed

--- app/dashboard/product/page.tsx
+++ app/dashboard/product/page.tsx
@@ -56,8 +56,30 @@ export default function ProductPage() {
         <div className="container mx-auto py-10">
             <div className="flex items-center justify-between">
                 <h1 className="text-2xl font-bold mb-4">Products</h1>
-                <div className="flex justify-between items-center mb-4">
-                    <Input className="w-64" placeholder="Search product name" type="search" />
+                <div className="mb-4">
+                    <div className="relative">
+                        <Input
+                            className="w-full pl-10 pr-4 py-2 border-2 border-gray-300 rounded-full text-sm focus:outline-none focus:border-blue-500"
+                            placeholder="Search product name"
+                            type="search"
+                        />
+                        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5 text-gray-400"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                                />
+                            </svg>
+                        </div>
+                    </div>
                 </div>
             </div>
             <Table>
