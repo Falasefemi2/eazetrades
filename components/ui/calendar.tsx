d735bec6cef3bf35cd11dce56f53f92322865549
Falasefemi2 <femifalase228@gmail.com>
2025-01-13 20:30:39 +0100

    rewrote the calender

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -2,12 +2,18 @@
 
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker } from "react-day-picker"
+import { DayPicker, DayPickerProps, CustomComponents } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 
-export type CalendarProps = React.ComponentProps<typeof DayPicker>
+export type CalendarProps = DayPickerProps
+
+// Extend the CustomComponents type to include IconLeft and IconRight
+interface ExtendedCustomComponents extends CustomComponents {
+  IconLeft?: React.ComponentType<React.SVGProps<SVGSVGElement>>
+  IconRight?: React.ComponentType<React.SVGProps<SVGSVGElement>>
+}
 
 function Calendar({
   className,
@@ -20,7 +26,8 @@ function Calendar({
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
       classNames={{
-        months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
+        months:
+          "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
         caption_label: "text-sm font-medium",
@@ -66,11 +73,12 @@ function Calendar({
         IconRight: ({ className, ...props }) => (
           <ChevronRight className={cn("h-4 w-4", className)} {...props} />
         ),
-      }}
+      } as ExtendedCustomComponents}
       {...props}
     />
   )
 }
+
 Calendar.displayName = "Calendar"
 
 export { Calendar }
